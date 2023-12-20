7f5f0836ce51e4ff834903d2b365aaa4e8e2743b
Andres pardo <fernando.andresp13@gmail.com>
2023-12-20 17:41:42 -0500

    se actualizo atributos de mensaje y controlador

--- src/main/java/com/andres/springboot/backend/chat/controllers/ChatController.java
+++ src/main/java/com/andres/springboot/backend/chat/controllers/ChatController.java
@@ -16,8 +16,10 @@ public class ChatController {
 	@SendTo("/chat/mensaje")//nombre del evento adonde vamos enviar el mensaje corresponde al configadurado en el config
 	public Mensaje recibeMensaje(Mensaje mensaje) {
 		mensaje.setFecha(new Date().getTime());
-		mensaje.setTexto("Recibido por el broker: "+ mensaje.getTexto());
 		
+		if(mensaje.getTipo().equals("NUEVO_USUARIO")){
+			mensaje.setTexto("nuevo usuario");
+		}
 		return mensaje;
 	}
 
