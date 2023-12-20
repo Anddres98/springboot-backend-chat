44ac5c4a0c771bed07efb2070409fcb63af86dba
Andres pardo <fernando.andresp13@gmail.com>
2023-12-20 11:08:36 -0500

    se agrego el Controler y la clase mensaje

---
+++ src/main/java/com/andres/springboot/backend/chat/controllers/ChatController.java
@@ -0,0 +1,24 @@
+package com.andres.springboot.backend.chat.controllers;
+
+import java.util.Date;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.andres.springboot.backend.chat.models.documents.Mensaje;
+
+@Controller
+public class ChatController {
+	
+	//Va recibir el mensaje del cliente
+	@MessageMapping("/mensaje")
+	@SendTo("/chat/mensaje")//nombre del evento adonde vamos enviar el mensaje corresponde al configadurado en el config
+	public Mensaje recibeMensaje(Mensaje mensaje) {
+		mensaje.setFecha(new Date().getTime());
+		mensaje.setTexto("Recibido por el broker: "+ mensaje.getTexto());
+		
+		return mensaje;
+	}
+
+}
